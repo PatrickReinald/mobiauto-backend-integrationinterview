951e6416b109c32e8e3f49c013d195cc3123129e
patricksantos-r5projetos <patrick.reinald@gmail.com>
2025-04-16 00:53:02 -0300

    Project initializer

---
+++ src/test/java/mobiauto/api/proximity_finder/ProximityFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package mobiauto.api.proximity_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
